a7370fbf7c5391ebd9c0217f242512f3ecf464d4
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 17:23:59 -0500

    cleanup the tests a lil bit

--- LazyPropertyHelperTests/LazyPropertyHelperTest.cs
+++ LazyPropertyHelperTests/LazyPropertyHelperTest.cs
@@ -5,69 +5,61 @@ namespace LazyPropertyHelperTests
 {
   public class LazyPropertyHelperTest
   {
+    private readonly int beforeInstancesCount = ExpensiveObject.InstancesCount;
+
+    private int CreatedInstances => ExpensiveObject.InstancesCount - beforeInstancesCount;
+    
     [Fact]
     public void LazyPropertyIsNotInitializedOnConstruction()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       new SampleService();
 
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount);
+      CreatedInstances.Should().Be(0);
     }
     
     [Fact]
     public void LazyPropertyIsInitializedWhenWorkNeedsToBeDone()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       new SampleService().DoWork(1);
 
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount + 1);
+      CreatedInstances.Should().Be(1);
     }
     
     [Fact]
     public void LazyPropertyIsInitializedWhenRead()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       var load = new SampleService().ExpensiveLoad;
 
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount + 1);
+      CreatedInstances.Should().Be(1);
     }
     
     [Fact]
     public void LazyPropertyIsInitializedWhenReadOnlyOnce()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       var service = new SampleService();
 
       var load1 = service.ExpensiveLoad;
       var load2 = service.ExpensiveLoad;
       var load3 = service.ExpensiveLoad;
 
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount + 1);
+      CreatedInstances.Should().Be(1);
     }
     
     [Fact]
     public void LazyPropertyIsInitializedOnlyOnce()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       var service = new SampleService();
       
       service.DoWork(1);
       service.DoWork(1);
       service.DoWork(1);
 
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount + 1);
+      CreatedInstances.Should().Be(1);
     }
 
     [Fact]
     public void LazyPropertyIsInitializedOncePerInstance()
     {
-      var beforeInstancesCount = ExpensiveObject.InstancesCount;
-
       var service1 = new SampleService();
       var service2 = new SampleService();
       
@@ -79,8 +71,7 @@ namespace LazyPropertyHelperTests
       service2.DoWork(1);
       service2.DoWork(1);
       
-      ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount + 2);
+      CreatedInstances.Should().Be(2);
     }
-    
   }
 }
