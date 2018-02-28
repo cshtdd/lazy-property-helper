d9b87dce6e080f642dce74495c41466beca38436
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 17:29:07 -0500

    properly implement the LazyPropertyHelper, the tests still pass

--- LazyPropertyHelper/LazyPropertyHelper.cs
+++ LazyPropertyHelper/LazyPropertyHelper.cs
@@ -4,5 +4,45 @@ namespace LazyPropertyHelper
 {
     public static class LazyPropertyHelper
     {
+        public static Func<T> Create<T>(Func<T> expensiveComputation) => new LazyPropertyHelperInternal<T>(expensiveComputation).Read;
+
+        private class LazyPropertyHelperInternal<T>
+        {
+            private readonly object _criticalSection = new object();
+            private Func<T> _expensiveComputationReader;
+
+            private readonly Func<T> _expensiveComputation;
+    
+            public T Read() => ExpensiveComputation;
+            private T ExpensiveComputation => _expensiveComputationReader();
+
+            public LazyPropertyHelperInternal(Func<T> expensiveComputation)
+            {
+                _expensiveComputation = expensiveComputation;
+                _expensiveComputationReader = CalculateAndCacheExpensiveComputation;
+            }     
+
+            private class CacheHolder
+            {
+                private readonly T _cachedResult;
+
+                public CacheHolder(Func<T> expensiveComputation) => _cachedResult = expensiveComputation();
+
+                public T Read() => _cachedResult;
+            }
+      
+            private T CalculateAndCacheExpensiveComputation()
+            {
+                lock (_criticalSection)
+                {
+                    if (_expensiveComputationReader == CalculateAndCacheExpensiveComputation)
+                    {
+                        _expensiveComputationReader = new CacheHolder(_expensiveComputation).Read;
+                    }
+                }
+      
+                return _expensiveComputationReader();
+            }
+        }
     }
 }
--- LazyPropertyHelperTests/SampleService.cs
+++ LazyPropertyHelperTests/SampleService.cs
@@ -1,19 +1,11 @@
+using System;
+
 namespace LazyPropertyHelperTests
 {
   public class SampleService
   {
-    private ExpensiveObject _expensiveLoad;
-    public ExpensiveObject ExpensiveLoad {
-      get
-      {
-        if (_expensiveLoad == null)
-        {
-          _expensiveLoad = new ExpensiveObject();
-        }
-
-        return _expensiveLoad;
-      }
-    }
+    private readonly Func<ExpensiveObject> _expensiveLoad = LazyPropertyHelper.LazyPropertyHelper.Create(() => new ExpensiveObject());
+    public ExpensiveObject ExpensiveLoad => _expensiveLoad();
       
     public void DoWork(int n)
     { 
