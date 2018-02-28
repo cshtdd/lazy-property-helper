10504325bc3ce9df66d098f2f8a50a95feed51fd
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 17:05:05 -0500

    continue capturing the behavior of the lazy property

--- LazyPropertyHelperTests/LazyPropertyHelperTest.cs
+++ LazyPropertyHelperTests/LazyPropertyHelperTest.cs
@@ -6,13 +6,23 @@ namespace LazyPropertyHelperTests
   public class LazyPropertyHelperTest
   {
     [Fact]
-    public void LazyPropertyIsInitializedOnlyWhenNeeded()
+    public void LazyPropertyIsNotInitializedOnConstruction()
     {
       var beforeInstancesCount = ExpensiveObject.InstancesCount;
 
       new SampleService();
 
       ExpensiveObject.InstancesCount.Should().Be(beforeInstancesCount);
+    }
+    
+    [Fact]
+    public void LazyPropertyIsInitializedWhenWorkNeedsToBeDone()
+    {
+      var beforeInstancesCount = ExpensiveObject.InstancesCount;
+
+      new SampleService().DoWork(1);
+
+      ExpensiveObject.InstancesCount.Should().BeGreaterThan(beforeInstancesCount);
     }
   }
 }
