947ac113cdbc3591781afe34d9de2c3ced513f73
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 17:15:57 -0500

    clean up unnecessary test infrastructure

--- LazyPropertyHelperTests/SampleService.cs
+++ LazyPropertyHelperTests/SampleService.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-
 namespace LazyPropertyHelperTests
 {
   public class SampleService
   {
-    public static volatile List<string> Operations = new List<string>();
-
-    public readonly string Id = Guid.NewGuid().ToString("N");
-
     private ExpensiveObject _expensiveLoad;
     public ExpensiveObject ExpensiveLoad {
       get
@@ -23,12 +16,8 @@ namespace LazyPropertyHelperTests
     }
       
     public void DoWork(int n)
-    {
-      Operations.Add($"{nameof(SampleService)}->DoWork; status:Start; {nameof(n)}:{n};");
-        
+    { 
       ExpensiveLoad.Move(n*10 - 100, n*10 + 100);
-        
-      Operations.Add($"{nameof(SampleService)}->DoWork; status:End; {nameof(n)}:{n};");
     }
   }
 }
